fa9f40632abedd289d30d0bdd2b3e24f8def17b8
Lester <lester@MacBook-Pro-de-Lester.local>
2023-05-10 22:01:38 -0300

    Primer commit

---
+++ src/test/java/com/educacionIT/productoApi/ProductoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educacionIT.productoApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
