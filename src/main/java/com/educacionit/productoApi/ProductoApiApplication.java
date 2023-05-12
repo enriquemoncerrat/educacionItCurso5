b4cb51b8ea886de76996ded4114f0acc225242f0
prgaggino <110431516+prgaggino@users.noreply.github.com>
2023-05-12 20:41:04 -0300

    commit

---
+++ src/main/java/com/educacionit/productoApi/ProductoApiApplication.java
@@ -0,0 +1,13 @@
+package com.educacionit.productoApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductoApiApplication.class, args);
+	}
+
+}
