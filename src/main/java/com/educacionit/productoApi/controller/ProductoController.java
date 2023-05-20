2b18f64e18bd410403da5d61a3409aca2c589021
prgaggino <110431516+prgaggino@users.noreply.github.com>
2023-05-19 21:37:35 -0300

    jpa

--- src/main/java/com/educacionit/productoApi/controller/ProductoController.java
+++ src/main/java/com/educacionit/productoApi/controller/ProductoController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.educacionit.productoApi.model.Producto;
 
+
+
 @RestController
 @RequestMapping("/producto")
 public class ProductoController {
@@ -14,7 +16,8 @@ public class ProductoController {
 	
 	@GetMapping("/get")
 	public ResponseEntity<Producto> getProducto() {
-		Producto producto = new Producto(300.2,"Papas","Verduras");
+		Producto producto = new Producto(1, 33.20 , "Papa", "Verdura");
+		
 		return ResponseEntity.ok(producto);
 		
 	}
--- src/main/java/com/educacionit/productoApi/model/Producto.java
+++ src/main/java/com/educacionit/productoApi/model/Producto.java
@@ -1,18 +1,70 @@
 package com.educacionit.productoApi.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Setter
+@Getter
+@Entity
+@Table(schema="curso5",name="producto")
 public class Producto {
 	
-	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long id;
 	private double precio;
+	@Column(name="nombre_producto",length=50)
 	private String nombre;
 	private String descripcion;
+	
+	
+	public Producto(long id, double precio, String nombre, String descripcion) {
+		super();
+		this.id = id;
+		this.precio = precio;
+		this.nombre = nombre;
+		this.descripcion = descripcion;
+	}
+
+
+	public long getId() {
+		return id;
+	}
+
+
+	public double getPrecio() {
+		return precio;
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 
